1debda434ca3e1624ca3d30f9be13461e7eebcb7
Ziad-Bouhlel <ziad.bouhlel@gmail.com>
2023-03-13 11:44:24 +0100

    new service

---
+++ front-end/src/mocks/quiz-list.mocks.ts
@@ -0,0 +1,15 @@
+import { Quiz } from '../models/quiz.model';
+...
+export const QUIZ_LIST: Quiz[] = [
+  {
+    id: '1',
+    name: 'Les Acteurs',
+    theme: 'Actor',
+    questions: [],
+  },
+  {
+    id: '2',
+    name: 'Les technos WEB',
+    questions: [],
+  }
+];
--- front-end/src/services/quiz.service.ts
+++ front-end/src/services/quiz.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { QUIZ_LIST } from '../mocks/quiz-list.mock';
+import {HttpClient} from "@angular/common/http";
+import {BehaviorSubject} from "rxjs";
 
 ...
 
@@ -8,6 +11,15 @@ import { Injectable } from '@angular/core';
 export class QuizService {
   //The list of quiz. The list is retrieved from the mock.
   private quizzes: Quiz[] = QUIZ_LIST; // Ici on initialise la valeur avec un mock QUIZ_LIST
+
   // The service's constructor. Le constructeur peut prendre en paramètre les dépendances du service - comme ici, HttpClient qui va permettre de récupérer les données d'un serveur
   constructor(private http: HttpClient) {}
+
+  //addQuizz() { … }
+
+  //deleteQuizz(id: string) { … }
+
+  // Observable containing a list of quizzes, initialized with an empty array.
+  // Naming convention: Add '$' at the end of the variable name to highlight it as an Observable.
+  public quizzes$: BehaviorSubject<Quiz[]> = new BehaviorSubject([]);
 }
