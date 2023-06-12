179b5a1d2193971aac5720637352f6f6fc465f07
ArtanWellis <elias78210m@gmail.com>
2023-06-12 15:02:39 +0200

    Suppression de l'user crée durant le test #87

--- front-end/e2e/e2e-component.fixture.ts
+++ front-end/e2e/e2e-component.fixture.ts
@@ -6,4 +6,13 @@ export class E2EComponentFixture {
   constructor(page: Page) {
     this.page = page;
   }
+
+ async fillForm(id: string, value: string): Promise<void> {
+   await this.page.fill("input[id=" + id + "]", value)
+ }
+
+ async clickCreate(): Promise<void> {
+   await this.page.click('button[type="submit"]')
+ }
+
 }
--- front-end/e2e/scenarios/testUser.spec.ts
+++ front-end/e2e/scenarios/testUser.spec.ts
@@ -2,27 +2,28 @@ import { test, expect } from '@playwright/test';
 import { testUrl } from 'e2e/e2e.config';
 import {serverUrl} from "../../src/configs/server.config";
 import {User} from "../../src/models/user.model";
+import {E2EComponentFixture} from "../e2e-component.fixture";
 
 // This file is here to test the playwright integration. Make the branch appear
 test.describe('Initial test display', () => {
   test('testCreation User', async ({ page }) => {
-    await page.goto(testUrl+"/user-list");
-    await page.fill('input[id="firstName"]', 'John');
-    await page.fill('input[id="lastName"]', 'Doe');
-    await page.fill('input[id="photoUrl"]', 'imageJohnDoe');
-    await page.click('button[type="submit"]');
-    await page.goto(testUrl+"/user-list");
+    let e2=new E2EComponentFixture(page)
+    await page.goto(testUrl + "/user-list");
+    await e2.fillForm("firstName", 'John');
+    await e2.fillForm("lastName", 'Doe');
+    await e2.fillForm("photoUrl", 'imageJohnDoe');
+    await e2.clickCreate();
+    await page.goto(testUrl + "/user-list");
     const user = await page.getByTestId('John Doe imageJohnDoe');
     const userFalse = await page.getByTestId('John Da imageJohnDoe');
-    expect(user).toHaveCount(1)
-    expect(userFalse).toHaveCount(0)
+    await expect(user).toHaveCount(1)
+    await expect(userFalse).toHaveCount(0);
+    await page.selectOption('select[id="id"]', {label: 'John Doe'});
+    await page.getByRole('button', {name: 'Supprimer le patient'}).click();
+    await expect(user).toHaveCount(0)
 
 
 
 
-
-
-
-    // Test case pass? Means the playwright setup is done! Congrats!
   });
 });
